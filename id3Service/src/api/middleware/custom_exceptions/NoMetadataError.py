559431c0ec1ee01b2edd76344f2a899fe3a35b1a
Christian <78870205+HaagChristian@users.noreply.github.com>
2024-02-20 19:27:11 +0100

    Delete id3Service directory

--- id3Service/src/api/middleware/custom_exceptions/NoMetadataError.py
+++
@@ -1,2 +0,0 @@
-class NoMetaDataError(Exception):
-    pass
--- id3Service/src/settings/error_messages.py
+++
@@ -1,7 +0,0 @@
-DB_INTEGRITY_ERROR: str = "Email already exists"
-
-DB_NO_RESULT_FOUND: str = "No data found"
-
-JWT_INVALID_TOKEN: str = "Provide a valid auth token"
-
-NO_METADATA_ERROR: str = "No ID3 tags found"
