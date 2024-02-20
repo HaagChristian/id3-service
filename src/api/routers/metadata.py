07412233e67664d071b3de0dd4a9e6329acaa5b5
christian <Christian@weinverkauf-haag.de>
2024-02-20 19:41:19 +0100

    improved error handling

--- src/api/routers/metadata.py
+++ src/api/routers/metadata.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
+from mutagen import MutagenError
 from starlette import status
 from starlette.responses import Response
 
@@ -19,5 +20,10 @@ def upload_file(response: Response, file: UploadFile = File(..., media_type="aud
             # specifies that the request was successful but some parts of the metadata are missing
             response.status_code = status.HTTP_206_PARTIAL_CONTENT
         return metadata
-    except NoMetaDataError as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e.args[0]))
+    except (MutagenError, NoMetaDataError, IOError) as e:
+        if type(e) == NoMetaDataError:
+            # 404 indicates normally that the specified resource was not found,
+            # but it is also possible to use it when requested data is missing (metadata)
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e.args[0]))
+        else:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e.args[0]))
--- src/service/extract_metadata.py
+++ src/service/extract_metadata.py
@@ -1,4 +1,5 @@
 from fastapi import UploadFile
+from mutagen import MutagenError
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 
@@ -8,16 +9,19 @@ from src.settings.error_messages import NO_METADATA_ERROR
 
 
 def extract_metadata_from_mp3_file(file: UploadFile):
-    audio = MP3(file.file, ID3=EasyID3)
-    if not audio.tags:
-        # there are no metadata tags in the file
-        raise NoMetaDataError(NO_METADATA_ERROR)
+    try:
+        audio = MP3(file.file, ID3=EasyID3)
+        if not audio.tags:
+            # there are no metadata tags in the file
+            raise NoMetaDataError(NO_METADATA_ERROR)
 
-    metadata_response = MetadataResponse(
-        title=audio.get('title', [None])[0],
-        artist=audio.get('artist', [None])[0],
-        album=audio.get('album', [None])[0],
-        genre=audio.get('genre', [None])[0],
-        date=audio.get('date', [None])[0]
-    )
-    return metadata_response
+        metadata_response = MetadataResponse(
+            title=audio.get('title', [None])[0],
+            artist=audio.get('artist', [None])[0],
+            album=audio.get('album', [None])[0],
+            genre=audio.get('genre', [None])[0],
+            date=audio.get('date', [None])[0]
+        )
+        return metadata_response
+    except(MutagenError, NoMetaDataError, IOError) as e:
+        raise e
